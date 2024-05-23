7b7fb7db5585b082980f2da6d5da3e01dd146809
age2pierre <pierre.j.robillard@gmail.com>
2024-05-23 14:33:46 +0200

    feat: add DynVirtualList and LazyImg

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
+import { DynVirtualList } from "./dyn-virtual-list";
+import { LazyImg } from "./lazy-img";
 
 const App: Component = () => {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwindcss!</p>
+    <>
+      <p class="text-4xl text-green-700 text-center py-20">
+        Hello tailwindcss!
+      </p>
+      <DynVirtualList
+        class="mx-auto"
+        dataStream={[...new Array(1085)].map(
+          (_, i) => `https://picsum.photos/id/${i}/300/200`
+        )}
+        overscan={5}
+        estimateSize={200}
+      >
+        {(item) => (
+          <div class="w-full h-[200px]">
+            <LazyImg src={item} alt="picsum dolor sit amet" class="mx-auto h-full max-w-[300px] text-center" />
+          </div>
+        )}
+      </DynVirtualList>
+    </>
   );
 };
 
